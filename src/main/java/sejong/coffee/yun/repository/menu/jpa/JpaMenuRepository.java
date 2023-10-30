c1f72e5be52825fb2bc5fcdf183aec8b500cda4a
Hayoon <hy97@sju.ac.kr>
2023-10-30 18:43:40 +0900

    feat(JpaMenuRepository): findById()를 통해 락을 해제하므로 해당 메서드에 @Lock(value = LockModeType.PESSIMISTIC_WRITE) 비관 락 선언

--- src/main/java/sejong/coffee/yun/repository/menu/jpa/JpaMenuRepository.java
+++ src/main/java/sejong/coffee/yun/repository/menu/jpa/JpaMenuRepository.java
@@ -1,7 +1,19 @@
 package sejong.coffee.yun.repository.menu.jpa;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import sejong.coffee.yun.domain.order.menu.Menu;
 
+import javax.persistence.LockModeType;
+import java.util.Optional;
+
 public interface JpaMenuRepository extends JpaRepository<Menu, Long> {
+
+    @NotNull
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    @Query("select m from Menu m where m.id = :id")
+    Optional<Menu> findById(@NotNull @Param("id") Long id);
 }
