b83c59ff05128b8778c79ffa2d77d7fdb83c827e
Hayoon <hy97@sju.ac.kr>
2023-11-05 17:48:33 +0900

    feat(MenuController): 메뉴검색(searchMenu) 및 인기메뉴 검색(searchRankingList)api 추가

--- src/main/java/sejong/coffee/yun/controller/MenuController.java
+++ src/main/java/sejong/coffee/yun/controller/MenuController.java
@@ -5,12 +5,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import sejong.coffee.yun.domain.order.menu.Menu;
+import sejong.coffee.yun.dto.menu.MenuDto;
 import sejong.coffee.yun.dto.menu.MenuPageDto;
+import sejong.coffee.yun.dto.menu.MenuRankingDto;
 import sejong.coffee.yun.mapper.CustomMapper;
 import sejong.coffee.yun.service.MenuService;
 
@@ -42,9 +45,9 @@ public class MenuController {
 
     @GetMapping("/page/{pageNum}")
     ResponseEntity<MenuPageDto.Response> findAll(@PathVariable int pageNum) {
-        PageRequest pr = PageRequest.of(pageNum, 5);
+        PageRequest pageRequest = PageRequest.of(pageNum, 5);
 
-        Page<Response> allMenu = menuService.findAll(pr);
+        Page<Response> allMenu = menuService.findAll(pageRequest);
         List<Response> responses = allMenu.stream().toList();
 
 
@@ -54,9 +57,9 @@ public class MenuController {
 
     @GetMapping("/caching/page/{pageNum}")
     ResponseEntity<MenuPageDto.Response> findAllByCaching(@PathVariable int pageNum) {
-        PageRequest pr = PageRequest.of(pageNum, 5);
+        PageRequest pageRequest = PageRequest.of(pageNum, 5, Sort.by("id"));
 
-        Page<Response> allMenu = menuService.findAllByCaching(pr);
+        Page<Response> allMenu = menuService.findAllByCaching(pageRequest);
         List<Response> responses = allMenu.stream().toList();
 
 
@@ -64,5 +67,19 @@ public class MenuController {
         return ResponseEntity.ok(response);
     }
 
+    @GetMapping("/search/menu")
+    ResponseEntity<MenuDto.Response> searchMenu(@RequestParam("title") String menuTitle) {
 
+        Response response = menuService.menuSearch(menuTitle);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/search/ranking-list")
+    ResponseEntity<List<MenuRankingDto.Response>> searchRankingList() {
+
+        List<MenuRankingDto.Response> responses = menuService.searchRankList();
+
+        return ResponseEntity.ok(responses);
+    }
 }
