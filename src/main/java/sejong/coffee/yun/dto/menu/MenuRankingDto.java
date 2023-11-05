cea66fa6861b2a45fa1e7c06dcdf712971aef1fb
Hayoon <hy97@sju.ac.kr>
2023-11-05 17:50:25 +0900

    feat(MenuRankingDto): redis zSet 변환 RankingDto 생성

---
+++ src/main/java/sejong/coffee/yun/dto/menu/MenuRankingDto.java
@@ -0,0 +1,24 @@
+package sejong.coffee.yun.dto.menu;
+
+import lombok.Getter;
+import org.springframework.data.redis.core.ZSetOperations;
+import sejong.coffee.yun.domain.order.menu.Menu;
+
+@Getter
+public class MenuRankingDto {
+
+    /**
+     * public record Response(String menuTitle, Double score) {
+     *
+     *         public static Response convertToResponseRankingDto(ZSetOperations.TypedTuple<String> typedTuple) {
+     *             return new Response(typedTuple.getValue(), typedTuple.getScore());
+     *         }
+     *     }
+     */
+    public record Response(String menuTitle, Double score, int viewCount, int orderCount) {
+        public static Response convertToResponseRankingDto(ZSetOperations.TypedTuple<String> typedTuple, Menu menu) {
+            return new Response(typedTuple.getValue(), typedTuple.getScore(), menu.getViewCount(), menu.getOrderCount());
+        }
+    }
+
+}
