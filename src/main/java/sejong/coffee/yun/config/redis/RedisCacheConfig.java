d49d626b0f960e3c83159a139007a5d32b41b35e
Hayoon <hy97@sju.ac.kr>
2023-11-11 20:09:43 +0900

    feat(RedisCacheConfig): ObjectRedisTemplate <String, Object> @Bean 등록

--- src/main/java/sejong/coffee/yun/config/redis/RedisCacheConfig.java
+++ src/main/java/sejong/coffee/yun/config/redis/RedisCacheConfig.java
@@ -1,6 +1,7 @@
 package sejong.coffee.yun.config.redis;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -34,8 +36,35 @@ public class RedisCacheConfig {
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper)))
-                .entryTtl(Duration.ofMinutes(30L));
+                .entryTtl(Duration.ofDays(1L));
 
         return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory).cacheDefaults(redisCacheConfiguration).build();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> responsePopularMenuRedisTemplate(RedisConnectionFactory redisConnectionFactory){
+        ObjectMapper mapper = getObjectMapper();
+        GenericJackson2JsonRedisSerializer redisSerializer = new GenericJackson2JsonRedisSerializer(mapper);
+
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(redisSerializer);
+
+        return redisTemplate;
+    }
+
+    private static ObjectMapper getObjectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.activateDefaultTyping(
+                BasicPolymorphicTypeValidator
+                        .builder()
+                        .allowIfSubType(Object.class)   //모든 객체의 타입정보를 저장할 수 있도록 설정
+                        .build(),
+                ObjectMapper.DefaultTyping.NON_FINAL
+        );
+        mapper.registerModule(new JavaTimeModule());    //LocaldateTime 저장을 위해 등록
+        mapper.disable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS);	//LocaldateTime을 Day까지 반환해줌
+        return mapper;
     }
 }
