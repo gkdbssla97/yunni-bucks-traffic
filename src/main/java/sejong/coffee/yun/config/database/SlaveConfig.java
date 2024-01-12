6ce1c17cf77173a75cd107cabbae7ca27af21509
Hayoon <hy97@sju.ac.kr>
2024-01-13 01:47:22 +0900

    feat(SlaveConfig): SlaveDB(MySQL) 컨피그 설정

--- src/main/java/sejong/coffee/yun/config/database/SlaveConfig.java
+++ src/main/java/sejong/coffee/yun/config/database/SlaveConfig.java
@@ -2,6 +2,7 @@ package sejong.coffee.yun.config.database;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +10,15 @@ import org.springframework.context.annotation.Configuration;
 import javax.sql.DataSource;
 
 @Configuration
+@EnableConfigurationProperties(DatabaseProperties.class)
 public class SlaveConfig {
+
+    public static final String TRANSACTION_MANAGER_BEAN_NAME = "slaveDBTransactionManager";
+    public static final String ENTITY_MANAGER_BEAN_NAME = "slaveDBEntityManager";
+    private static final String DATASOURCE_BEAN_NAME = "slaveDataSource";
+    private static final String DATASOURCE_PROPERTIES_PREFIX = "spring.datasource.slave";
+    private static final String DATASOURCE_PROPERTIES = "slaveDataSourceProperties";
+    private static final String HIBERNATE_PROPERTIES = "slaveHibernateProperties";
 
     @Bean(name = "slaveDataSource")
     @ConfigurationProperties(prefix="spring.datasource.slave.hikari")
