cd78d468a6155abbba5f44d039ad8def71c8263e
Hayoon <hy97@sju.ac.kr>
2024-02-18 17:18:00 +0900

    refactor(MenuService): Write-Back 캐싱전략 CompletableFuture 비동기 처리로 로직 변경

--- src/main/java/sejong/coffee/yun/service/MenuService.java
+++ src/main/java/sejong/coffee/yun/service/MenuService.java
@@ -22,6 +22,7 @@ import sejong.coffee.yun.repository.menu.MenuRepository;
 import sejong.coffee.yun.util.wrapper.RestPage;
 
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
 
 import static sejong.coffee.yun.dto.menu.MenuRankingDto.Response;
 
@@ -103,24 +104,40 @@ public class MenuService {
                 .toList();
     }
 
+    /**
+     * 1. keys.stream().map(key -> ...)): 각 키에 대해 비동기 작업을 생성하고, 그 결과를 futures라는 리스트에 저장한다. CompletableFuture.runAsync()를 사용하여 각 작업을 별도의 스레드에서 비동기로 실행
+     * 2. Optional.ofNullable((Menu) objectRedisTemplate.opsForValue().get(key)): 주어진 키에 대한 값(메뉴)를 Redis에서 반환한다. 가져온 메뉴가 null이 아닌 경우에만 이후의 코드 실행
+     * 3. Optional.ofNullable(menuRepository.findByTitle(popularMenu.getTitle())): 가져온 메뉴의 제목과 일치하는 메뉴를 데이터베이스에서 조회한다. 찾은 메뉴가 null이 아닌 경우에만 이후의 코드 실행
+     * 4. Double score = redisTemplate.opsForZSet().score("ranking", popularMenu.getTitle()): Redis의 "ranking" ZSet에서 가져온 메뉴의 제목에 해당하는 점수를 반환
+     * 5. Optional.ofNullable(score).ifPresent(sc -> ...)): 가져온 점수가 null이 아닌 경우에만 updateMenu 메서드를 호출
+     * 6. CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).thenRunAsync(() -> redisTemplate.delete("menu::*")): 모든 비동기 작업이 완료되면 Redis의 "menu::*" 패턴에 매칭되는 모든 키를 삭제한다. 이 작업도 별도의 스레드에서 비동기로 실행
+     */
     @Scheduled(cron = "0 0 0 * * *") // 매일 00시 00분에 실행
-    @Transactional
     public void refreshPopularMenusInRedis() {
-        Optional.ofNullable(redisTemplate.keys("menu::"))
-                .ifPresent(keys -> keys.stream()
-                        .map(key -> (Menu) objectRedisTemplate.opsForValue().get(key))
-                        .filter(Objects::nonNull)
-                        .forEach(popularMenu -> {
-                            Menu findMenu = menuRepository.findByTitle(popularMenu.getTitle());
-                            Double score = redisTemplate.opsForZSet().score("ranking", popularMenu.getTitle());
-                            if (score != null && findMenu != null) {
-                                findMenu.updatePopularScoreByWritingBack(popularMenu.getOrderCount(), popularMenu.getViewCount(), score);
-                            } else {
-                                menuRepository.save(findMenu);
-                            }
-                        })
-                );
+        Set<String> keys = Optional.ofNullable(redisTemplate.keys("menu::*")).orElse(Collections.emptySet());
 
-        redisTemplate.delete("menu::*");
+        // CompletableFuture를 사용한 비동기 처리
+        List<CompletableFuture<Void>> futures = keys.stream()
+                .map(key -> CompletableFuture.runAsync(() -> {
+                    Optional.ofNullable((Menu) objectRedisTemplate.opsForValue().get(key))
+                            .ifPresent(popularMenu -> {
+                                Optional.ofNullable(menuRepository.findByTitle(popularMenu.getTitle()))
+                                        .ifPresent(findMenu -> {
+                                            Double score = redisTemplate.opsForZSet().score("ranking", popularMenu.getTitle());
+                                            Optional.ofNullable(score)
+                                                    .ifPresent(sc -> updateMenu(findMenu, popularMenu.getOrderCount(), popularMenu.getViewCount(), sc));
+                                        });
+                            });
+                })).toList();
+
+        // 모든 비동기 작업이 완료되면 redisTemplate.delete("menu::*") 작업을 실행
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+                .thenRunAsync(() -> redisTemplate.delete("menu::*"));
+    }
+
+    // 각 작업이 별도의 트랜잭션에서 실행되며, 하나의 작업이 실패하더라도 다른 작업에는 영향을 주지 않는다.
+    @Transactional
+    public void updateMenu(Menu menu, int orderCount, int viewCount, double score) {
+        menu.updatePopularScoreByWritingBack(orderCount, viewCount, score);
     }
 }
