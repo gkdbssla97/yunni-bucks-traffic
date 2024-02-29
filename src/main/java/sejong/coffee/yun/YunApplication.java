c6e19048f9182d6fe41009bdf46ab4ca466fa975
Hayoon <hy97@sju.ac.kr>
2024-02-29 16:28:52 +0900

    feat(): war 설정

--- src/main/java/sejong/coffee/yun/YunApplication.java
+++ src/main/java/sejong/coffee/yun/YunApplication.java
@@ -3,13 +3,20 @@ package sejong.coffee.yun;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
 @EnableScheduling
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class YunApplication {
+public class YunApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(YunApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(YunApplication.class, args);
