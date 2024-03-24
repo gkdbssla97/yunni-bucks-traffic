2ae778e8fa9713eb07f96ea98a740c9f79023044
Hayoon <hy97@sju.ac.kr>
2024-03-25 01:27:32 +0900

    docs(Jenkinsfile): retry SMTP email notification

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -56,7 +56,6 @@ pipeline {
             script {
                 emailext (
                         to: 'hy97@sju.ac.kr',
-                        cc: 'gkdbssla97@gmail.com',
                         subject: "STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
                         body: """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
              <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
