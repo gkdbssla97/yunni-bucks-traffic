63b5530550d05209b9eca5b01d04248f1a82a8a0
Hayoon <hy97@sju.ac.kr>
2024-04-02 03:50:38 +0900

    docs(build.gradle): test includeTestsMatching

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -24,6 +24,7 @@ pipeline {
                 }
             }
         }
+
         stage('deploy') {
             steps {
 //                deploy adapters: [tomcat9(credentialsId: 'deployer_user', path: '', url: 'http://43.201.8.89:8080/')], contextPath: null, war: '**/*.war'
